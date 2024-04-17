cd3570bc7c03cc474cc6ec558fcecd5fc3ce1dda
Apicella Nazareno <apicellanazareno1@gmail.com>
2024-04-16 22:49:01 -0300

    escribi el codigo

---
+++ area-cuadrado.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+  float lado1, lado2, area
+    printf("ingrese el alto en cm");
+    scanf("%f", &lado1);
+    printf("ingrese el ancho en cm");
+    scanf("%f", &lado2);
+
+   area= lado1*lado2;
+
+    printf("su area en cm cuadrados es %e/n", area);
+
+ return 0;
+
+
+
+}
